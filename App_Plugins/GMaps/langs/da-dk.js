1577492834cfb6c65132188c6d91b6a5f993e92e
Jan Skovgaard <jaskov@gmail.com>
2014-04-22 16:39:35 +0200

    Initial commit of language files.

---
+++ App_Plugins/GMaps/langs/da-dk.js
@@ -0,0 +1,5 @@
+{
+	"geoCodeTrouble":"Ups! Der var et problem med at finde adressen! Prøv venligst igen.",
+	"locationSet":"Adresse sat til",
+	"resetTxt":"Nulstil position"
+}
---
+++ App_Plugins/GMaps/langs/de-de.js
@@ -0,0 +1,5 @@
+{
+	"geoCodeTrouble":"Oops! Having trouble geocoding specified address! Please try again.",
+	"locationSet":"Location set to",
+	"resetTxt":"Reset position"
+}
